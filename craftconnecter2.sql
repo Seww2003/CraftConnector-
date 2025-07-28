c95de10bed8402e0240befff7919fbf9f22576d1
Seww2003 <162076901+Seww2003@users.noreply.github.com>
2025-07-28 22:58:02 +0530

    Add files via upload

---
+++ craftconnecter2.sql
@@ -0,0 +1,115 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 28, 2025 at 06:32 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `craftconnecter2`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin_table`
+--
+-- Error reading structure for table craftconnecter2.admin_table: #1932 - Table &#039;craftconnecter2.admin_table&#039; doesn&#039;t exist in engine
+-- Error reading data for table craftconnecter2.admin_table: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `craftconnecter2`.`admin_table`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `brands`
+--
+-- Error reading structure for table craftconnecter2.brands: #1932 - Table &#039;craftconnecter2.brands&#039; doesn&#039;t exist in engine
+-- Error reading data for table craftconnecter2.brands: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `craftconnecter2`.`brands`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `card_details`
+--
+-- Error reading structure for table craftconnecter2.card_details: #1932 - Table &#039;craftconnecter2.card_details&#039; doesn&#039;t exist in engine
+-- Error reading data for table craftconnecter2.card_details: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `craftconnecter2`.`card_details`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `categories`
+--
+-- Error reading structure for table craftconnecter2.categories: #1932 - Table &#039;craftconnecter2.categories&#039; doesn&#039;t exist in engine
+-- Error reading data for table craftconnecter2.categories: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `craftconnecter2`.`categories`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders_pending`
+--
+-- Error reading structure for table craftconnecter2.orders_pending: #1932 - Table &#039;craftconnecter2.orders_pending&#039; doesn&#039;t exist in engine
+-- Error reading data for table craftconnecter2.orders_pending: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `craftconnecter2`.`orders_pending`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products`
+--
+-- Error reading structure for table craftconnecter2.products: #1932 - Table &#039;craftconnecter2.products&#039; doesn&#039;t exist in engine
+-- Error reading data for table craftconnecter2.products: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `craftconnecter2`.`products`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `registration_tbl`
+--
+-- Error reading structure for table craftconnecter2.registration_tbl: #1932 - Table &#039;craftconnecter2.registration_tbl&#039; doesn&#039;t exist in engine
+-- Error reading data for table craftconnecter2.registration_tbl: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `craftconnecter2`.`registration_tbl`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `remove_cart`
+--
+-- Error reading structure for table craftconnecter2.remove_cart: #1932 - Table &#039;craftconnecter2.remove_cart&#039; doesn&#039;t exist in engine
+-- Error reading data for table craftconnecter2.remove_cart: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `craftconnecter2`.`remove_cart`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_orders`
+--
+-- Error reading structure for table craftconnecter2.user_orders: #1932 - Table &#039;craftconnecter2.user_orders&#039; doesn&#039;t exist in engine
+-- Error reading data for table craftconnecter2.user_orders: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `craftconnecter2`.`user_orders`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_payments`
+--
+-- Error reading structure for table craftconnecter2.user_payments: #1932 - Table &#039;craftconnecter2.user_payments&#039; doesn&#039;t exist in engine
+-- Error reading data for table craftconnecter2.user_payments: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `craftconnecter2`.`user_payments`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_table`
+--
+-- Error reading structure for table craftconnecter2.user_table: #1932 - Table &#039;craftconnecter2.user_table&#039; doesn&#039;t exist in engine
+-- Error reading data for table craftconnecter2.user_table: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `craftconnecter2`.`user_table`&#039; at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
